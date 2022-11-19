e93494665e2a56fa838af13e3cc8ffc006f5a5fe
Avinash-0514 <100390020+Avinash-0514@users.noreply.github.com>
2022-11-19 17:52:59 +0530

    Clock
    Clock projects

---
+++ script.js
@@ -0,0 +1,22 @@
+
+const clockframe = document.querySelector('.clock');
+function runClock(){
+    var time= new Date(); 
+var hrs=time.getHours();
+var mins=time.getMinutes();
+var sec=time.getSeconds();
+var txt="AM";
+if (hrs>12){
+    hrs=hrs-12;
+    txt="PM";
+}else if(hrs==0){
+    hrs=12;
+txt="AM";
+}
+hrs=hrs<10?'0'+hrs:hrs;
+mins=mins<10?'0'+mins:mins;
+sec=sec<10?'0'+sec:sec;
+clockframe.innerHTML = `${hrs} : ${mins} : ${sec}`;
+}
+runClock();
+setInterval(runClock,1000);
